acec336af9d898fe588b11c328f53fb7f39d7fa9
Evgeny Eroshin <eee333@yandex.ru>
2022-04-17 12:20:15 +0300

    Create model User(AbstractUser)

--- core/models.py
+++ core/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    MEMBER = "member"
+    MODERATOR = "moderator"
+    ADMIN = "admin"
+    ROLES = [
+        (MEMBER, "Пользователь"),
+        (MODERATOR, "Модератор"),
+        (ADMIN, "Админ"),
+    ]
+
+    email = models.EmailField(null=True)
+    role = models.CharField(max_length=9, choices=ROLES, default="member")
+    # age = models.PositiveIntegerField(null=True)
+    birth_date = models.DateField(null=True, default="2000-01-01")
+    locations = models.ManyToManyField(Location)
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+        ordering = ["username"]
+
+    def __str__(self):
+        return self.username
