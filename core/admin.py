5f0cb26791be8ea792c97f4f5b0a83c4eb3a263a
Evgeny Eroshin <eee333@yandex.ru>
2022-04-25 15:51:35 +0300

    Correct admin.py

--- core/admin.py
+++ core/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from core.models import User
 
 
-class UserAdmin(admin.ModelAdmin):
+class UserAdmin(BaseUserAdmin):
     # exclude = ("password",)
     list_display = ("username", "email", "first_name", "last_name")
     readonly_fields = ("date_joined", "last_login")
