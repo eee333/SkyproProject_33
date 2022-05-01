d1e1d7004efc7108e60deb2164ed5223e7b821f2
Evgeny Eroshin <eee333@yandex.ru>
2022-05-02 00:31:59 +0300

    Change url

--- core/urls.py
+++ core/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from core import views
 
 urlpatterns = [
-    path('profile/<int:pk>', views.ProfileView.as_view()),
+    path('profile', views.ProfileView.as_view()),
     path('signup', views.UserCreateView.as_view()),
     path('login', views.LoginView.as_view()),
     path('update_password', views.UserUpdatePassView.as_view()),
