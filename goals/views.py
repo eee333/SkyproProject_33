d46645d4782195d5116e5809b61e2bb869cbe77e
Evgeny Eroshin <eee333@yandex.ru>
2022-05-10 01:45:47 +0300

    Create Goal func

--- goals/views.py
+++ goals/views.py
@@ -1,22 +1,24 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import OrderingFilter, SearchFilter
 from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.permissions import IsAuthenticated
 
-from goals.models import GoalCategory
-from goals.serializers import GoalCreateSerializer, GoalCategorySerializer
+from goals.filters import GoalDateFilter
+from goals.models import GoalCategory, Goal
+from goals.serializers import GoalCategoryCreateSerializer, GoalCategorySerializer, GoalCreateSerializer, GoalSerializer
 
 
 class GoalCategoryCreateView(CreateAPIView):
     model = GoalCategory
+    serializer_class = GoalCategoryCreateSerializer
     permission_classes = [IsAuthenticated]
-    serializer_class = GoalCreateSerializer
 
 
 class GoalCategoryListView(ListAPIView):
     model = GoalCategory
-    permission_classes = [IsAuthenticated]
     serializer_class = GoalCategorySerializer
+    permission_classes = [IsAuthenticated]
     pagination_class = LimitOffsetPagination
     filter_backends = [
         OrderingFilter,
@@ -45,3 +47,44 @@ class GoalCategoryView(RetrieveUpdateDestroyAPIView):
         instance.is_deleted = True
         instance.save()
         return instance
+
+
+class GoalCreateView(CreateAPIView):
+    model = Goal
+    serializer_class = GoalCreateSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class GoalView(RetrieveUpdateDestroyAPIView):
+    model = Goal
+    serializer_class = GoalSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Goal.objects.filter(user=self.request.user)
+
+    def perform_destroy(self, instance):
+        instance.status = Goal.Status.archived
+        instance.save()
+        return instance
+
+
+class GoalListView(ListAPIView):
+    model = Goal
+    serializer_class = GoalSerializer
+    permission_classes = [IsAuthenticated]
+    pagination_class = LimitOffsetPagination
+    filter_backends = [
+        DjangoFilterBackend,
+        OrderingFilter,
+        SearchFilter,
+    ]
+    filterset_class = GoalDateFilter
+    ordering_fields = ["due_date", "priority"]
+    ordering = ["title"]
+    search_fields = ["title", "description"]
+
+    def get_queryset(self):
+        return Goal.objects.filter(
+            user=self.request.user,
+        )
--- todolist/settings.py
+++ todolist/settings.py
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'social_django',
-    'django_filters'
+    'django_filters',
     'core',
     'goals',
 ]
