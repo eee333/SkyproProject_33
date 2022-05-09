d072922e134051a4bb1178eda2edf163d15cb30c
Evgeny Eroshin <eee333@yandex.ru>
2022-05-09 20:07:21 +0300

    Add goal_category/list

--- goals/views.py
+++ goals/views.py
@@ -1,11 +1,23 @@
-from rest_framework.generics import CreateAPIView
-from rest_framework import permissions
+from rest_framework.generics import CreateAPIView, ListAPIView
+from rest_framework.permissions import IsAuthenticated
 
 from goals.models import GoalCategory
-from goals.serializers import GoalCreateSerializer
+from goals.serializers import GoalCreateSerializer, GoalCategorySerializer
 
 
 class GoalCategoryCreateView(CreateAPIView):
     model = GoalCategory
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [IsAuthenticated]
     serializer_class = GoalCreateSerializer
+
+
+class GoalCategoryListView(ListAPIView):
+    model = GoalCategory
+    permission_classes = [IsAuthenticated]
+    serializer_class = GoalCategorySerializer
+
+    def get_queryset(self):
+        return GoalCategory.objects.filter(
+            user=self.request.user,
+            is_deleted=False,
+        )
