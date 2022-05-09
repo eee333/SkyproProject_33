a5d2bce915dcccad34d09736e066184b6e3ae07c
Evgeny Eroshin <eee333@yandex.ru>
2022-05-09 21:19:05 +0300

    Add goal_category RetrieveUpdateDestroyAPIView

--- goals/admin.py
+++ goals/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from goals.models import GoalCategory
+
+
+class GoalCategoryAdmin(admin.ModelAdmin):
+    list_display = ("title", "user", "created", "updated")
+    search_fields = ("title", "user")
+    readonly_fields = ("created", "updated")
+    list_filter = ("is_deleted",)
+
+
+admin.site.register(GoalCategory, GoalCategoryAdmin)
