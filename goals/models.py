aeb699738caac0bee122fcc37a6e460f5d220743
Evgeny Eroshin <eee333@yandex.ru>
2022-05-09 23:59:28 +0300

    Create Goal model

--- goals/models.py
+++ goals/models.py
@@ -4,7 +4,21 @@ from django.utils import timezone
 from core.models import User
 
 
-class GoalCategory(models.Model):
+class DatesModelMixin(models.Model):
+    class Meta:
+        abstract = True  # Помечаем класс как абстрактный – для него не будет таблички в БД
+
+    created = models.DateTimeField(verbose_name="Дата создания")
+    updated = models.DateTimeField(verbose_name="Дата последнего обновления")
+
+    def save(self, *args, **kwargs):
+        if not self.id:  # Когда модель только создается – у нее нет id
+            self.created = timezone.now()
+        self.updated = timezone.now()  # Каждый раз, когда вызывается save, проставляем свежую дату обновления
+        return super().save(*args, **kwargs)
+
+
+class GoalCategory(DatesModelMixin):
     class Meta:
         verbose_name = "Категория"
         verbose_name_plural = "Категории"
@@ -12,11 +26,47 @@ class GoalCategory(models.Model):
     title = models.CharField(verbose_name="Название", max_length=255)
     user = models.ForeignKey(User, verbose_name="Автор", on_delete=models.PROTECT)
     is_deleted = models.BooleanField(verbose_name="Удалена", default=False)
-    created = models.DateTimeField(verbose_name="Дата создания")
-    updated = models.DateTimeField(verbose_name="Дата последнего обновления")
 
-    def save(self, *args, **kwargs):
-        if not self.id:  # Когда объект только создается, у него еще нет id
-            self.created = timezone.now()  # проставляем дату создания
-        self.updated = timezone.now()  # проставляем дату обновления
-        return super().save(*args, **kwargs)
+
+class Goal(DatesModelMixin):
+    class Meta:
+        verbose_name = "Цель"
+        verbose_name_plural = "Цели"
+
+    class Status(models.IntegerChoices):
+        to_do = 1, "К выполнению"
+        in_progress = 2, "В процессе"
+        done = 3, "Выполнено"
+        archived = 4, "Архив"
+
+    class Priority(models.IntegerChoices):
+        low = 1, "Низкий"
+        medium = 2, "Средний"
+        high = 3, "Высокий"
+        critical = 4, "Критический"
+
+    title = models.CharField(verbose_name="Название", max_length=255)
+    description = models.TextField(
+        verbose_name="Описание",
+        null=True,
+        blank=True,
+        default=None,
+        max_length=1000,
+    )
+    category = models.ForeignKey(GoalCategory, verbose_name="Категория", on_delete=models.PROTECT)
+    user = models.ForeignKey(User, verbose_name="Автор", related_name="goals", on_delete=models.PROTECT)
+    status = models.PositiveSmallIntegerField(
+        verbose_name="Статус", choices=Status.choices, default=Status.to_do
+    )
+    priority = models.PositiveSmallIntegerField(
+        verbose_name="Приоритет", choices=Priority.choices, default=Priority.medium
+    )
+    due_date = models.DateField(
+        verbose_name="Дедлайн",
+        null=True,
+        blank=True,
+        default=None,
+    )
+
+    def __str__(self):
+        return self.title
